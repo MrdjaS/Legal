30e79d26204641d58c8bb3e054185d27c9862fb7
MrdjaS <mrdjas96@hotmail.com>
2022-06-15 08:51:50 +0200

    Working on slider section

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,7 @@
 // Sticky Navbar
 
+const header = document.querySelector('header');
 const navbar = document.querySelector('.navbar');
-const iconMenu = document.querySelector('.icon-menu');
-const iconClose = document.querySelector('.icon-close');
-const headerContainer = document.querySelector('.header__container');
 const navbarLogoFirst = document.getElementById('navbar__logo--primary');
 const navbarLogoSecond = document.getElementById('navbar__logo--secondary');
 let sticky = navbar.offsetTop;
@@ -12,19 +10,13 @@ window.onscroll = function() {myFunction()};
 
 function myFunction() {
     if (window.pageYOffset >= sticky) {
-      navbar.classList.add('sticky');
+      header.classList.add('sticky');
       navbarLogoFirst.classList.remove('active');
       navbarLogoSecond.classList.add('active');
-      iconMenu.classList.add('menu-active');
-      iconClose.classList.add('menu-active');
-      headerContainer.classList.add('container--active');
     } else {
-      navbar.classList.remove('sticky');
+      header.classList.remove('sticky');
       navbarLogoFirst.classList.add('active');
       navbarLogoSecond.classList.remove('active');
-      iconMenu.classList.remove('menu-active');
-      iconClose.classList.remove('menu-active');
-      headerContainer.classList.remove('container--active');
     }
 }
 
@@ -36,8 +28,53 @@ const navbarMenu = document.querySelector('.navbar__menu');
 
 [closeBtn, menuBtn].forEach(item => {
     item.addEventListener('click', () => {
-        closeBtn.classList.toggle('active');
-        menuBtn.classList.toggle('active');
-        navbarMenu.classList.toggle('active');
+      [closeBtn, menuBtn, navbarMenu].forEach(btn => {
+        btn.classList.toggle('active');
+      });
     });
+});
+
+
+//Slider 
+
+const slider = document.querySelector('.slider__content');
+const btnPrev = document.querySelector('.slider__prev-Arrow');
+const btnNext = document.querySelector('.slider__next-Arrow');
+const card = document.querySelector('.slider__card');
+const cards = document.querySelectorAll('.slider__card');
+const indicators = [... document.querySelectorAll('.slider__indicator')];
+const sliderParagraph = [... document.querySelectorAll('.slider__paragraph--responsive')];
+let cardWidth = card.offsetWidth;
+let windowWidth = window.innerWidth;
+let numOfSlides = cards.length - 1;
+let slideNumber = 0;
+
+btnNext.addEventListener('click',() => {
+    if (slideNumber < numOfSlides) {
+        slideNumber++;
+    } else {
+      return slideNumber;
+    }
+    for(let i = 0; i < indicators.length; i++) {
+      indicators[i].classList.remove('indicator__active');
+      sliderParagraph[i].classList.remove('paragraph__active');
+    }
+    slider.style.transform = `translateX(-${slideNumber*cardWidth}px)`;
+    indicators[slideNumber].classList.add('indicator__active');
+    sliderParagraph[slideNumber].classList.add('paragraph__active');
+});
+  
+btnPrev.addEventListener('click',() => {
+    if (slideNumber > 0) {
+        slideNumber--;
+    } else {
+      return slideNumber;
+    }
+    for(let i = 0; i < indicators.length; i++) {
+      indicators[i].classList.remove('indicator__active');
+      sliderParagraph[i].classList.remove('paragraph__active');
+    }
+    slider.style.transform = `translateX(-${slideNumber*cardWidth}px)`;
+    indicators[slideNumber].classList.add('indicator__active');
+    sliderParagraph[slideNumber].classList.add('paragraph__active');
 });
